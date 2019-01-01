a8b7f73fe77af7c0422e969a2a060456ffcc86b7
Ken Ding <aenkil@gmail.com>
2019-01-01 21:53:18 +0800

    Add title, meta tag to each screen

--- src/js/screens/details/View.js
+++ src/js/screens/details/View.js
@@ -1,6 +1,15 @@
 /* @flow */
 import React from 'react';
+import Helmet from 'react-helmet-async';
 
-const Details = () => <div>Details page</div>;
+const Details = () => (
+  <div>
+    <Helmet>
+      <title>Details Page - Tiny React Kit</title>
+      <meta name="description" content="keywords for details page" />
+    </Helmet>
+    <p>Details page</p>
+  </div>
+);
 
 export default Details;
--- src/js/screens/home/View.js
+++ src/js/screens/home/View.js
@@ -1,17 +1,24 @@
 /* @flow */
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import { Link } from '@reach/router';
+import Helmet from 'react-helmet-async';
 
 const Section = styled.h1`
   color: white;
   background: ${props => props.background};
 `
 const Home = () => (
-  <Section background="cornflowerblue">
-    <div>Home page</div>
-    <Link to="details">Details Page</Link>
-  </Section>
+  <Fragment>
+    <Helmet>
+      <title>Home Page - Tiny React Kit</title>
+      <meta name="description" content="keywords for home page" />
+    </Helmet>
+    <Section background="cornflowerblue">
+      <div>Home page</div>
+      <Link to="details">Details Page</Link>
+    </Section>
+  </Fragment>
 );
 
 export default Home;
